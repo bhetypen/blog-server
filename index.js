b2f1d87dfd1e78658efaf86baf6d665b19e327a8
bhety.tech <bhety.tech@gmail.com>
2025-10-22 01:13:11 +0200

    added corsoption

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended:true}));
 
 //Allow specific origins
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5174', 'http://localhost:5173', 'http://localhost:5174','http://localhost:5175'],
+    origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5174', 'http://localhost:5173', 'http://localhost:5174','http://localhost:5175','https://blog-frontend-sandy-nine.vercel.app', 'https://blog-frontend-git-master-bhety-ps-projects.vercel.app'],
     credentials: true,
     optionsSuccessStatus: 200
 };
